589269979efd4f3dc9ffb42248dae0e47a55986d
kohdz <umair.stem@gmail.com>
2019-12-13 14:24:44 -0500

    Form Validators Part II

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Feedback, ContactType } from '../shared/feedback'
 
@@ -13,7 +13,8 @@ export class ContactComponent implements OnInit {
 
   feedbackForm: FormGroup;
   feedback: Feedback;
-  contactType = ContactType
+  contactType = ContactType;
+  @ViewChild('fform', {static: false}) feedbackFormDirective;
 
   constructor(private fb: FormBuilder) { 
     this.createForm();
@@ -24,21 +25,30 @@ export class ContactComponent implements OnInit {
   }
 
   createForm() {
-    this.feedbackForm = this.fb.group ({
+    this.feedbackForm = this.fb.group({
+      firstname: ['', Validators.required ],
+      lastname: ['', Validators.required ],
+      telnum: ['', Validators.required ],
+      email: ['', Validators.required ],
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset({
       firstname: '',
       lastname: '',
-      telnum: 0,
+      telnum: '',
       email: '',
       agree: false,
       contacttype: 'None',
       message: ''
-    })
-  }
-
-  onSubmit() {
-    this.feedback = this.feedbackForm.value
-    console.log(this.feedback)
-    this.feedbackForm.reset();
+    });
+    this.feedbackFormDirective.resetForm();
   }
 
 
