e77bab809daa8544c577de11df92e1e453eb717e
kohdz <umair.stem@gmail.com>
2020-03-13 17:06:41 -0400

    Basic Service Implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { AppComponent } from "./app.component";
 
 import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { DishService } from "./services/dish.service";
 
 @NgModule({
   declarations: [AppComponent, MenuComponent, DishdetailComponent],
@@ -28,7 +29,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
