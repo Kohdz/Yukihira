3ab8fc149a4196497317d9346b744456d0c346e4
kohdz <umair.stem@gmail.com>
2020-03-13 18:34:52 -0400

    Angular Routing Implemented / Home. About and Contact Compoment Implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { MatListModule } from "@angular/material/list";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
-import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
 
@@ -15,11 +14,25 @@ import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
 import { DishService } from "./services/dish.service";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
