2c390b1ee43693553d7edefa532f7b4ac7077658
kohdz <umair.stem@gmail.com>
2020-03-14 22:27:52 -0400

    Used Observables to Implement Next Dish Feature

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Dish } from "../shared/dish";
 import { DishService } from "../services/dish.service";
 import { Params, ActivatedRoute, ActivationEnd } from "@angular/router";
 import { Location } from "@angular/common";
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-dishdetail",
@@ -11,17 +12,38 @@ import { Location } from "@angular/common";
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor(
-    private dishService: DishService,
+    private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location
   ) {}
 
   ngOnInit() {
-    const id = this.route.snapshot.params["id"];
+    this.dishservice
+      .getDishIds()
+      .subscribe(dishIds => (this.dishIds = dishIds));
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => this.dishservice.getDish(params["id"]))
+      )
+      .subscribe(dish => {
+        this.dish = dish;
+        this.setPrevNext(dish.id);
+      });
+  }
 
-    this.dishService.getDish(id).subscribe(dish => (this.dish = dish));
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[
+      (this.dishIds.length + index - 1) % this.dishIds.length
+    ];
+    this.next = this.dishIds[
+      (this.dishIds.length + index + 1) % this.dishIds.length
+    ];
   }
 
   goBack(): void {
