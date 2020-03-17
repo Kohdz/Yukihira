c337908cd7554aeff54498f97fb2b636c3e901bd
kohdz <umair.stem@gmail.com>
2020-03-16 22:14:20 -0400

    e2e tests setup

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,17 @@ describe("workspace-project App", () => {
     page = new AppPage();
   });
 
-  it("should display welcome message", () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual("Yukihira app is running!");
+  it("should display message saying Yukihira Resturant", () => {
+    page.navigateTo("/");
+    expect(page.getParagraphText("app-root-h1")).toEqual("Resturant Yukihira");
+  });
+
+  it("should navagate to about us page by clicking on the link", () => {
+    page.navigateTo("/");
+    let navlink = page.getAllElements("a").get(1);
+    navlink.click();
+
+    expect(page.getParagraphText("h3")).toBe("About Us");
   });
 
   afterEach(async () => {
@@ -24,5 +32,19 @@ describe("workspace-project App", () => {
         level: logging.Level.SEVERE
       } as logging.Entry)
     );
+  });
+  it("it should enter a new comment for the first dish", () => {
+    page.navigateTo("/dishdetail/0");
+
+    const newAuthor = page.getElement("input[type=text]");
+    newAuthor.sendKeys("Test Author");
+
+    const newComment = page.getElement("textarea");
+    newComment.sendKeys("Test Comment");
+
+    const newSubmitButton = page.getElement("button[type=submit]");
+    newSubmitButton.click();
+
+    browser.pause();
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,19 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element } from "protractor";
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  navigateTo(link: string) {
+    return browser.get(link);
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
+  }
+
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
   }
 }
