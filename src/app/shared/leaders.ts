adaf3205a2aa292ae92b6358dee051c439e4af10
kohdz <umair.stem@gmail.com>
2020-03-18 16:07:05 -0400

    About Page UI/UX fixed

--- src/app/shared/leaders.ts
+++ src/app/shared/leaders.ts
@@ -1,46 +1,46 @@
 export const LEADERS = [
   {
     id: "0",
-    name: "Jiro",
+    name: "Sukiyabashi Jiro",
     image: "/assets/images/starChef.jpg",
     designation: "Chief Epicurious Officer",
     abbr: "CEO",
     featured: true,
     // tslint:disable-next-line:max-line-length
     description:
-      "Our CEO, Peter, credits his hardworking East Asian immigrant parents who undertook the arduous journey to the shores of America with the intention of giving their children the best future. ."
+      "Our CEO, Jiro, credits his hardworking East Asian parents. He started cooking at the age of 7. Although he became a chef after retirement, Jiro is credited with innovating Japanese food with a unique Russian and French influence"
   },
   {
     id: "1",
-    name: "Dhanasekaran Witherspoon",
-    image: "/assets/images/yuko.png",
+    name: "Nobu Matsuhisa",
+    image: "/assets/images/Nobu.jpg",
     designation: "Chief Food Officer",
     abbr: "CFO",
     featured: false,
     // tslint:disable-next-line:max-line-length
     description:
-      "Our CFO, Danny, as he is affectionately referred to by his colleagues, comes from a long established family tradition in farming and produce. His experiences growing up on a farm in the Australian outback gave him great appreciation for varieties of food sources. As he puts it in his own words, Everything that runs, wins, and everything that stays, pays!"
+      "Our CFO, Nobu, as he is affectionately referred to by his colleagues, comes from a long established family tradition in farming and produce. His experiences growing up on a farm in the Australian outback gave him great appreciation for varieties of food sources. As he puts it in his own words, Everything that runs, wins, and everything that stays, pays!"
   },
   {
     id: "2",
-    name: "Agumbe Tang",
-    image: "/assets/images/yuko.png",
+    name: "Harumi Kurihara",
+    image: "/assets/images/Harumi.jpg",
     designation: "Chief Taste Officer",
     abbr: "CTO",
     featured: false,
     // tslint:disable-next-line:max-line-length
     description:
-      "Blessed with the most discerning gustatory sense, Agumbe, our CFO, personally ensures that every dish that we serve meets his exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet his exacting standards. He lives by his motto, You click only if you survive my lick."
+      "Blessed with the most discerning gustatory sense, Harumi, our CFO, personally ensures that every dish that we serve meets her exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet his exacting standards. She lives by her motto, You click only if you survive my lick."
   },
   {
     id: "3",
-    name: "Alberto Somayya",
-    image: "/assets/images/yuko.png",
+    name: "Rika Yukimasa",
+    image: "/assets/images/Rija.jpg",
     designation: "Executive Chef",
     abbr: "EC",
     featured: false,
     // tslint:disable-next-line:max-line-length
     description:
-      "Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. He says, Put together the cuisines from the two craziest cultures, and you get a winning hit! Amma Mia!"
+      "Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, she specializes in creating mouthwatering French-Russian fusion experiences. She says, Put together the cuisines from the two craziest cultures, and you get a winning hit!"
   }
 ];
